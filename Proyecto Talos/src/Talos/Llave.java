7d51e87d3fcb88e6a097800f65600544ee35ca88
Joriks <jorikst@gmail.com>
2014-10-07 19:22:40 +0200

    Commit inicial del proyecto y creación de clase Llave
    Commit inicial con la creación del proyecto sobre eclipse y jdk 1.7
    
    Clase Llave implementada y generada la documentación interna

---
+++ Proyecto Talos/src/Talos/Llave.java
@@ -0,0 +1,42 @@
+package Talos;
+
+/**
+ * Clase Llave, dispone de un código numérico que permite identificarla.
+ * @version 0.1 06/10/2014
+ * @author Grupo Talos { Jorge Bote Albalá, Juan Jose Ramón Rodríguez }
+ */
+public class Llave implements Comparable<Llave>{
+	
+	/** Identificador de la clase Llave */
+	int id;
+	
+	public Llave() {
+		id = 0;
+	}
+	
+	public Llave(int id){
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(id);
+	}
+
+	@Override
+	public int compareTo(Llave o) {
+		if(this.id < o.id)
+			return -1;
+		if(this.id > o.id)
+			return 1;
+		return 0;
+	}
+}
