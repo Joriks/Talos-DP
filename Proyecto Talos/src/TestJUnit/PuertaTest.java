2a58255a904d201bab1f60a04922439593da34ae
Jorge Bote Albala <jorikst@gmail.com>
2014-10-12 00:40:47 +0200

    Añadida libreria JUnit y Test base de Arbol, Llave y Puerta.
    Creadas las clases base para los test bajo JUnit de Arbol, Llave y
    Puerta.

---
+++ Proyecto Talos/src/TestJUnit/PuertaTest.java
@@ -0,0 +1,14 @@
+package TestJUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PuertaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
