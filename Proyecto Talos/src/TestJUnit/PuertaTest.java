8dea179e732dcb403fa66e39b01f67f5c622a201
Jorge Bote Albala <jorikst@gmail.com>
2014-10-13 17:53:18 +0200

    Test JUnit de Puerta implementado

--- Proyecto Talos/src/TestJUnit/PuertaTest.java
+++ Proyecto Talos/src/TestJUnit/PuertaTest.java
@@ -4,11 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Talos.Estados;
+import Talos.Llave;
+import Talos.Puerta;
+
 public class PuertaTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Puerta p1 = new Puerta();
+		int[] combi1 = {1,2,3,4,5};
+		int[] combi2 = {1,5,6,8,9,10};
+		Puerta p2 = new Puerta(combi2, 3);
+		
+		assertTrue(p1.estadoPuerta() == Estados.NoConfigurada);
+		assertFalse(p2.estadoPuerta() == Estados.NoConfigurada);
+		assertFalse(p2.estadoPuerta() == Estados.Abierta);
+		
+		p1.configurarPuerta(combi1, 3);
+		assertTrue(p1.estadoPuerta() == Estados.Cerrada);
+		
+		p1.probarLlave(new Llave(4));
+		p1.probarLlave(new Llave(2));
+		p1.probarLlave(new Llave(5));
+		
+		assertTrue(p1.estadoPuerta() == Estados.Abierta);
+		
+		assertTrue(p2.probarLlave(new Llave(5)));
+		assertFalse(p2.probarLlave(new Llave(2)));
+		assertFalse(p2.probarLlave(new Llave(5)));
+		
+		assertTrue(p2.estadoPuerta() == Estados.Cerrada);
+		
+		
 	}
 
 }
