dd541518cd1e512691d5ab7cee291a78c421255c
Juan José Ramón Rodríguez <jramonro@alumnos.unex.es>
2014-10-30 12:24:21 +0100

    Añadido puerta, implementación simular turno, reparación fallos y doc.
    Añadido el atributo puerta a la sala, implementado los metodos
    configurarPuerta y obtenerPuerta.
    
    Implementación del metodo simularTurno.
    
    Corrección de errores y documentación básica

--- Proyecto Talos/src/Talos/Sala.java
+++ Proyecto Talos/src/Talos/Sala.java
@@ -1,58 +1,103 @@
 package Talos;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.TreeSet;
 
 import Robots.Robot;
 
 /**
- * Clase Puerta, guarda información sobre la cerradura de salida del laberinto.
- * @version 0.2 06/10/2014
+ * Clase Sala, guarda información sobre una sala del laberinto.
+ * @version 0.2 30/10/2014
  * @author Grupo Talos { Jorge Bote Albalá, Juan Jose Ramón Rodríguez }
  */
 public class Sala {
+	/** Guarda el identificador de la sala*/
 	private int id_sala;
-	private TreeSet<Llave> llaves_sala;
-	private LinkedList<Robot> robot_sala;
+	/** Llaves guardadas en la sala*/
+	private LinkedList<Llave> llaves_sala;
+	/** Robots que se encuentran en la sala*/
+	private LinkedList<Robot> robots_sala;
+	/** Información de la puerta, si la tiene*/
+	Puerta puerta;
 	
 	public Sala(int id){
 		id_sala = id;
-		llaves_sala = new TreeSet<Llave>();
-		robot_sala = new LinkedList<Robot>();
+		llaves_sala = new LinkedList<Llave>();
+		robots_sala = new LinkedList<Robot>();
+		puerta = null;
 	}
 	
-	public Sala(int id, TreeSet<Llave> llaves_sala){
+	public Sala(int id, LinkedList<Llave> llaves_sala){
 		id_sala = id;
 		this.llaves_sala = llaves_sala;
-		robot_sala = new LinkedList<Robot>();
+		robots_sala = new LinkedList<Robot>();
+		puerta = null;
 	}
 	
-	//Como se podría hacer con la llave como parámetro
-	public Llave sacarLlave(){
+	public Puerta obtenerPuerta(){
+		return puerta;
+	}
+	
+	/**
+	 * Metodo para configurar inicialmente la puerta de la sala.
+	 * @param combinacion Combinación con la que configurar la puerta
+	 * @param altura Altura del árbol con la que configurar la puerta
+	 * Complejidad: O(n)
+	 */
+	public void configurarPuerta(int[] combinacion, int altura){
+		puerta = new Puerta(combinacion, altura);
+	}
+	
+	/**
+	 * Metodo que saca y devuelve la primera llave a sacar
+	 * @return la llave que saca
+	 * Complejidad: O(1)
+	 */
+	public Llave sacarLlave(){	//Como se podría hacer con la llave como parámetro
 		return llaves_sala.pollFirst();
 	}
 	
+	/**
+	 * Metodo que introduce una llave en la sala 
+	 * @param llave Llave a meter en la sala
+	 * Complejidad: O(1)
+	 */
 	public void meterLlave(Llave llave){
 		llaves_sala.add(llave);
 	}
 	
+	/**
+	 * Metodo que saca y devuelve el primer robot a salir de la sala
+	 * @return Robot El robot que sale
+	 * Complejidad: O(1)
+	 */
 	public Robot sacarRobot(){
-		return robot_sala.removeFirst();
+		return robots_sala.removeFirst();
 	}
 	
-	public void meterRobot(){
-		robot_sala.pollLast();
+	/**
+	 *  Metodo que introduce un robot en la sala
+	 *  Complejidad: O(1)
+	 */
+	public void meterRobot(Robot robot){
+		robots_sala.addLast(robot);
 	}
 	
-	public void simularTurno(){
-		//TODO
+	/**
+	 * Metodo para actualizar el turno de los robots de la sala.
+	 * @param turno_actual Información sobre el turno actual en la simulación
+	 * Complejidad: O(n log n)
+	 */
+	public void simularTurno(int turno_actual){
+		for(Robot robot : robots_sala)
+			robot.simularTurno(turno_actual);
 	}
 	
 	@Override
 	public String toString() {
 		return "(sala:" + id_sala + ":" + llaves_sala.toString() + ")";
 	}
-
 
 	public static void main(String[] args) {
 		Sala s= new Sala(1);
