04af1b3f513431be04e9869ed0ac554ea7ad3c65
Jorge Bote Albala <jorikst@gmail.com>
2014-11-05 21:22:34 +0100

    Modificada simulación de turno de los robots, asi como sus acciones.
    Robot
    Eliminado metodo generarRuta, ahora se asigna una ruta estática despues
    de crear el robot mediante asignarRuta.
    
    Modificado simularTurno, tiene en cuenta si está en una sala con puerta
    y si debe moverse o no, también mueve el robot a la sala especial si se
    dan las condiciones.
    
    Modificado interactuarPuerta para capturarlas excepciones si se queda
    sin llaves.
    
    Modificado moverRobot solucionando fallo de movimiento al Este y Oeste,
    además ahora el robot sale de la sala antigua y entra en la nueva
    consecutivamente.
    
    El metodo incrementarTurno cambia su modificador a protegido.
    
    Implementados metodos auxiliares (obtenerSala y obtenerMarca).
    
    Asimo
    Añadido conjunto de llaves iniciales.
    
    Implementado metodo simularTurno con las acciones de asimo, el
    movimiento es ininterrumpido.
    
    Modificado interactuarLlave para capturarlas excepciones si se queda sin
    llaves y soltar llaves solo en salas pares.

--- Proyecto Talos/src/Robots/Asimo.java
+++ Proyecto Talos/src/Robots/Asimo.java
@@ -1,5 +1,7 @@
 package Robots;
 
+import Talos.Laberinto;
+import Talos.Llave;
 import Talos.Puerta;
 import Talos.Sala;
 
@@ -12,20 +14,62 @@ public class Asimo extends Robot{
 
 	public Asimo() {
 		super("asimo",'\0', 0, 0);
+		int[] comb = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29};
+		for(int id_llave : comb)
+			llaves.add(new Llave(id_llave));
 	}
 	
 	public Asimo(char marca, int turno, int sala_actual){
 		super("asimo", marca, turno, sala_actual);
+		int[] comb = {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29};
+		for(int id_llave : comb)
+			llaves.add(new Llave(id_llave));
 	}
 	
+	/**
+	 * Simula la ejecución de un turno del robot
+	 * PRE:
+	 * POST:
+	 * Complejidad: O(log n)
+	 * @param laberinto 
+	 */
+	public void simularTurno(Laberinto laberinto, int turno_actual){
+		if(this.turno == turno_actual){
+			if(laberinto.tienePuerta(sala_actual))
+				interactuarPuerta(laberinto.obtenerPuerta(sala_actual));
+			moverRobot(laberinto);
+			interactuarLlave(laberinto.obtenerSala(sala_actual));
+			incrementarTurno();
+		}
+	}
+	
+	/**
+	 * Accion a ejecutar sobre la puerta.
+	 * @param puerta, puerta sobre la que se va a ejecutar una acción
+	 * PRE:
+	 * POST:
+	 * Complejidad: O(log n)
+	 */
 	@Override
 	protected void interactuarPuerta(Puerta puerta) {
 		System.out.println(nombre + ":" + marca + ":" + "Cierro puerta");
+		puerta.cerrarPuerta();
 	}
 
+	/**
+	 * Ejecuta una accion de una llave sobre una sala
+	 * @param sala, sala sobre la que se va a realizar la acción de la llave.
+	 * PRE:
+	 * POST:
+	 * Complejidad: O(1).
+	 */
 	@Override
 	protected void interactuarLlave(Sala sala) {
-		sala.meterLlave(llaves.pop());
-		System.out.println(nombre + ":" + marca + ":" + "Suelto llave");
+		try {
+			if(sala_actual%2==0)
+				sala.meterLlave(llaves.pop());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 }
